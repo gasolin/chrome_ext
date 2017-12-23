2984c21b342bd03b7727a05ee13f94067747bdfb
Fred Lin <gasolin@gmail.com>
2017-12-23 23:13:05 +0800

    rewrote body.js

--- src/script/pageload/body.js
+++ src/script/pageload/body.js
@@ -1,42 +1,30 @@
-
-
 //injects various information on the page
 var body = function(){
     var url = document.location.href;
-    var isOnGitHubcom = url.indexOf('://github.com') != -1 && url.indexOf('://github.com') < 15;
-    var isOnGitHubEnterprise = document.getElementsByClassName('enterprise').length >= 1 && document.getElementsByClassName('header-logo-invertocat').length >= 1;
-    var isOnGitHub = isOnGitHubEnterprise || isOnGitHubcom;
+    var isOnGitHub = url.indexOf('://github.com') != -1 && url.indexOf('://github.com') < 15;
 
     if(!isOnGitHub){
         setThumbnail('');
-    }
-
-    var isOnUserProfile = isOnGitHub && url.match(/.+\/.+\/?/gi) != null && document.getElementById("report-block-modal");
-    var isOnProfile = isOnGitHub && url.match(/.+\/.+\/.+\/?/gi) != null;
-    var isOnRepo = isOnGitHub && url.match(/.+\/.+\/.+\/.+\/?/gi) != null;
-    var isOnIssuePage = isOnGitHub && ( url.indexOf('/pull/') != -1 || url.indexOf('/issue/') != -1 || url.indexOf('/issues/') != -1 );
-    var isOnIssuesPage = isOnGitHub && url.indexOf('/issues') != -1;
-    var isAlreadyGitcoinBountyD = document.getElementsByClassName('gitcoin_bounty').length >= 1;
-    var isOnIssueBoard = isOnGitHub && url.indexOf('boards') != -1;
-    
-    if(isOnUserProfile){
-        addButtonToUserPage();
-    }
-    if (isOnIssuePage){
+    } else {
+        // isOnUserProfile
+        if (url.match(/.+\/.+\/?/gi) != null && document.getElementById("report-block-modal")) {
+            addButtonToUserPage();
+        }
+        // isOnIssuePage
+        if (url.indexOf('/pull/') != -1 || url.indexOf('/issue/') != -1 || url.indexOf('/issues/') != -1) {
             addButtonToIssuePage();
             addBountyInfoToIssuePage(url);
             injectGetBountyAmount();
-    } else if (isOnRepo){
-        var repoUrl = document.location.href.split('issues')[0].split('pulls')[0];
-        injectGetNumberBounties(repoUrl);
-    } else if (isOnGitHub){
-            injectGetTotalBounties();
-    } 
-    if (isOnIssuesPage) {
+        } else if (url.match(/.+\/.+\/.+\/.+\/?/gi) != null) { // isOnRepo
+            var repoUrl = document.location.href.split('issues')[0].split('pulls')[0];
+            injectGetNumberBounties(repoUrl);
+        } else if (url.indexOf('/issues') != -1) { // isOnIssuesPage
             injectGetAllBountiesOnIssuesPage();
-    }
-    if (isOnIssueBoard) {
-            injectGetAllBountiesOnIssueBoard(); 
+        } else if (url.indexOf('boards') != -1) { // isOnIssueBoard
+            injectGetAllBountiesOnIssueBoard();
+        } else {
+            injectGetTotalBounties();
+        }
     }
 }
 
